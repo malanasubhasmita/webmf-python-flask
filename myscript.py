fac40fd58f4567aaf0dae75d493597fbc7c5a721
malanasubhasmita <36625976+malanasubhasmita@users.noreply.github.com>
2020-02-06 16:41:07 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,3 +1,8 @@
 print("this line is printed")
 ip = '192.168.12.42'
 print(ip)
+
+def run():
+    prepare("this is a duplicate")  # Noncompliant - "this is a duplicate" is duplicated 3 times
+    execute("this is a duplicate")
+    release("this is a duplicate")
